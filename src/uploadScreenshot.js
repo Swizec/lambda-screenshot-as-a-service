af533221a1ee374e49341cc2b76c2f1301590f79
Swizec Teller <swizec@swizec.com>
2018-11-17 22:58:12 -0800

    Use gm for resizing

--- src/uploadScreenshot.js
+++ src/uploadScreenshot.js
@@ -3,33 +3,35 @@ const fs = require('fs');
 const imagemin = require('imagemin');
 const imageminJpegtran = require('imagemin-jpegtran');
 const imageminPngquant = require('imagemin-pngquant');
-const ImageminGm = require('imagemin-gm');
-
-const imageminGm = new ImageminGm();
-
+const gm = require('gm');
 
 exports.uploadScreenshot = async function uploadScreenshot(path) {
     const screenshot = await imagemin([path], {
         plugins: [imageminJpegtran(), imageminPngquant({
             quality: '65-80'
-        }), imageminGm.resize({
-            width: 480
         })]
-    })
-
-    const s3 = new aws.S3({
-        apiVersion: '2006-03-01',
     });
 
-    const {
-        Location
-    } = await s3.upload({
-        Bucket: 'techletter.app',
-        Key: `screenshot-${new Date().getTime()}.png`,
-        Body: screenshot[0].data,
-        ACL: 'public-read',
-    }).promise();
+    return new Promise((resolve, reject) => {
+            gm(screenshot[0].data).resize(480).toBuffer('png', async function (err, buffer) {
+                if (err) reject(err);
+                const s3 = new aws.S3({
+                    apiVersion: '2006-03-01',
+                });
 
+                const {
+                    Location
+                } = await s3.upload({
+                    Bucket: 'techletter.app',
+                    Key: `screenshot-${new Date().getTime()}.png`,
+                    Body: buffer,
+                    ACL: 'public-read',
+                }).promise();
 
-    return Location;
-};
+                resolve(Location);
+            })
+        }
+
+        // return Location;
+    )
+}
