d0cac267b98ed16b8bae8c36dd3a4040996f3796
Swizec Teller <swizec@swizec.com>
2018-11-17 23:30:22 -0800

    Cleanup code

--- src/uploadScreenshot.js
+++ src/uploadScreenshot.js
@@ -1,42 +1,28 @@
 const aws = require("aws-sdk");
-const fs = require("fs");
-// const imagemin = require('imagemin');
-// const imageminJpegtran = require('imagemin-jpegtran');
-// const imageminPngquant = require('imagemin-pngquant');
 const gm = require("gm").subClass({
     imageMagick: true
 });
 
 exports.uploadScreenshot = async function uploadScreenshot(path) {
-    // const screenshot = await imagemin([path], {
-    //     plugins: [imageminJpegtran(), imageminPngquant({
-    //         quality: '65-80'
-    //     })]
-    // });
-
-    return new Promise(
-        (resolve, reject) => {
-            gm(path)
-                .resize(480)
-                .toBuffer("png", async function(err, buffer) {
-                    if (err) reject(err);
-                    const s3 = new aws.S3({
-                        apiVersion: "2006-03-01"
-                    });
-
-                    const { Location } = await s3
-                        .upload({
-                            Bucket: "techletter.app",
-                            Key: `screenshot-${new Date().getTime()}.png`,
-                            Body: buffer,
-                            ACL: "public-read"
-                        })
-                        .promise();
-
-                    resolve(Location);
+    return new Promise((resolve, reject) => {
+        gm(path)
+            .resize(480)
+            .toBuffer("png", async function(err, buffer) {
+                if (err) reject(err);
+                const s3 = new aws.S3({
+                    apiVersion: "2006-03-01"
                 });
-        }
 
-        // return Location;
-    );
+                const { Location } = await s3
+                    .upload({
+                        Bucket: "techletter.app",
+                        Key: `screenshot-${new Date().getTime()}.png`,
+                        Body: buffer,
+                        ACL: "public-read"
+                    })
+                    .promise();
+
+                resolve(Location);
+            });
+    });
 };
